7d4a2d7bd456785d2ffa6ea0b54f6799d33f5adb
罗高 <luogao_lg@sina.com>
2018-05-01 00:03:28 +0800

    update dot demo

--- dot/dot.js
+++ dot/dot.js
@@ -1,34 +1,38 @@
-class dot {
-  constructor(x, y, vx, vy, canvas, bgColor = 'lightgray') {
-    this.time = 2
+class Dot {
+  constructor(x, y, vx, vy, canvas, bgColor = '#990033') {
+    this.time = 1
     this.x = x
     this.y = y
     this.vx = vx
     this.vy = vy
     this.bgColor = bgColor
-    this.size = Math.ceil(Math.random() * 3 + 2)
+    this.size = Math.ceil(Math.random() * 3 + 5)
     this.canvas = canvas
-    this.ctx = null
+    this.ctx = canvas.getContext('2d')
   }
-  render(ctx) {
-    const { x, y, size, bgColor } = this
+
+  render() {
+    const { x, y, ctx, bgColor, size, canvas } = this
     ctx.save()
-    this.ctx = ctx
-    this.ctx.beginPath()
-    this.ctx.fillStyle = bgColor
-    this.ctx.arc(x - size / 2, y - size / 2, size, 0, Math.PI * 2)
-    this.ctx.closePath()
-    this.ctx.fill()
-    this.ctx.restore()
+    ctx.beginPath()
+    ctx.fillStyle = bgColor
+    ctx.arc(x - size / 2, y - size / 2, size, 0, Math.PI * 2)
+    ctx.closePath()
+    ctx.fill()
+    ctx.restore()
   }
+
   update() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.x = this.x + this.vx * this.time;
-    this.y = this.y + this.vy * this.time;
-    this.vx = (this.x < this.canvas.width && this.x > 0) ? this.vx : (-this.vx);
-    this.vy = (this.y < this.canvas.height && this.y > 0) ? this.vy : (-this.vy);
-    this.render(this.ctx);
+    const { ctx, canvas, size } = this
+    const { width, height } = canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    this.x = this.x + this.vx * this.time
+    this.y = this.y + this.vy * this.time
+    this.vx = (this.x < width && this.x > 0) ? this.vx : (-this.vx)
+    this.vy = (this.y < height && this.y > 0) ? this.vy : (-this.vy)
+    this.render(ctx)
   }
+
 }
 
-export default dot
+export default Dot
--- dot/index.js
+++ dot/index.js
@@ -1,29 +1,37 @@
 import { createCanvas } from '../utils'
 import Dot from './dot'
 
+window.requestAnimFrame = (function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60)
+    }
+})()
+
 window.onload = function () {
-
-  const canvas = createCanvas(500, 500, 'myCanvas')
-  const ctx = canvas.getContext('2d')
-  const num = 2
-  let dots = []
-
+  const canvas = createCanvas(window.innerWidth, window.innerHeight)
+  const num = 5
+  const dots = []
 
   for (let i = 0; i < num; i++) {
     let x = Math.ceil(Math.random() * canvas.width)
     let y = Math.ceil(Math.random() * canvas.height)
     let vx = Math.ceil(Math.random() * 2)
     let vy = Math.ceil(Math.random() * 2)
-    const d = new Dot(x, y, vx, vy, canvas)
-    d.render(ctx)
+    let d = new Dot(x, y, vx, vy, canvas)
+    d.render()
     dots.push(d)
   }
 
-  const anim = () => {
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].update()
+  requestAnimFrame(anim)
+  function anim() {
+    for (var i = 0; i < dots.length; i++) {
+      (i => {
+        dots[i].update()
+      })(i)
     }
-    window.requestAnimationFrame(anim)
+    requestAnimFrame(anim)
   }
-  window.requestAnimationFrame(anim)
 }
