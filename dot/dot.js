95ba4817322768320c4d05791d3f3df1ed0267f2
罗高 <luogao_lg@sina.com>
2018-05-01 00:22:05 +0800

    find the bug & fixed it

--- dot/dot.js
+++ dot/dot.js
@@ -1,31 +1,31 @@
 class Dot {
-  constructor(x, y, vx, vy, canvas, bgColor = '#990033') {
+  constructor(x, y, vx, vy, canvas) {
     this.time = 1
     this.x = x
     this.y = y
     this.vx = vx
     this.vy = vy
-    this.bgColor = bgColor
+    this.bgColor = () => `#${(~~(Math.random() * (1 << 24))).toString(16)}`
     this.size = Math.ceil(Math.random() * 3 + 5)
     this.canvas = canvas
-    this.ctx = canvas.getContext('2d')
+    this.ctx = {}
   }
 
-  render() {
-    const { x, y, ctx, bgColor, size, canvas } = this
+  render(ctx) {
+    const { x, y, bgColor, size, canvas } = this
     ctx.save()
-    ctx.beginPath()
-    ctx.fillStyle = bgColor
-    ctx.arc(x - size / 2, y - size / 2, size, 0, Math.PI * 2)
-    ctx.closePath()
-    ctx.fill()
-    ctx.restore()
+    this.ctx = ctx
+    this.ctx.beginPath()
+    this.ctx.fillStyle = bgColor()
+    this.ctx.arc(x - size / 2, y - size / 2, size, 0, Math.PI * 2)
+    this.ctx.closePath()
+    this.ctx.fill()
+    this.ctx.restore()
   }
 
   update() {
     const { ctx, canvas, size } = this
     const { width, height } = canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
     this.x = this.x + this.vx * this.time
     this.y = this.y + this.vy * this.time
     this.vx = (this.x < width && this.x > 0) ? this.vx : (-this.vx)
--- dot/index.js
+++ dot/index.js
@@ -12,7 +12,8 @@ window.requestAnimFrame = (function () {
 
 window.onload = function () {
   const canvas = createCanvas(window.innerWidth, window.innerHeight)
-  const num = 5
+  const ctx = canvas.getContext('2d')
+  const num = 20
   const dots = []
 
   for (let i = 0; i < num; i++) {
@@ -21,16 +22,15 @@ window.onload = function () {
     let vx = Math.ceil(Math.random() * 2)
     let vy = Math.ceil(Math.random() * 2)
     let d = new Dot(x, y, vx, vy, canvas)
-    d.render()
+    d.render(ctx)
     dots.push(d)
   }
 
   requestAnimFrame(anim)
   function anim() {
-    for (var i = 0; i < dots.length; i++) {
-      (i => {
-        dots[i].update()
-      })(i)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].update()
     }
     requestAnimFrame(anim)
   }
