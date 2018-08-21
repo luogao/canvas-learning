780409248d019250bdd691699c11680a8d4df2e9
luogao <luogao_lg@sina.com>
2018-08-21 17:06:37 +0800

    update audio-wave demo

---
+++ audio-wave/README.md
@@ -0,0 +1,7 @@
+# audio-wave
+
+## RE
+  - [基于Web Audio API实现音频可视化效果](https://developer.mozilla.org/zh-CN/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API)
+  - [HTML5 Audio: createMediaElementSource breaks audio output](https://stackoverflow.com/questions/39187924/html5-audio-createmediaelementsource-breaks-audio-output/39192187#39192187)
+  - [AnalyserNode](https://developer.mozilla.org/zh-CN/docs/Web/API/AnalyserNode)
+  - [web audio api](https://developer.mozilla.org/zh-CN/docs/Web/API/Web_Audio_API)
--- audio-wave/index.js
+++ audio-wave/index.js
@@ -1,1 +1,86 @@
 import './index.css'
+
+import { createCanvas } from '../utils'
+
+const modeSelector = document.getElementById('mode')
+modeSelector.addEventListener('change', function () {
+  const val = this.value
+  if (val === '1') {
+    requestAnimationFrame(draw)
+  } else {
+    requestAnimationFrame(draw2)
+  }
+}, false)
+const W = 500
+const H = 500
+
+const canvas = createCanvas(W, H)
+const ctx = canvas.getContext('2d')
+const myAudio = document.querySelector('audio')
+
+// 获取web audio 上下文对象
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+// 获取声音源
+const source = audioCtx.createMediaElementSource(myAudio)
+// 获取分析对象
+const analyser = audioCtx.createAnalyser()
+// 设置
+analyser.fftSize = 256
+const bufferLength = analyser.fftSize
+
+const dataArray = new Uint8Array(bufferLength)
+
+// 连接解析器
+source.connect(analyser)
+// 输出音频
+source.connect(audioCtx.destination)
+
+const draw = () => {
+  // 获取当前声音的波形；将当前波形，或者时域数据拷贝进 Uint8Array数组（无符号字节数组）
+  analyser.getByteTimeDomainData(dataArray)
+  ctx.clearRect(0, 0, W, H)
+  ctx.fillStyle = 'rgb(200,200,200)'
+  ctx.fillRect(0, 0, W, H)
+  ctx.strokeStyle = 'rgb(0,0,0)'
+  ctx.beginPath()
+  const sliceWidth = W * 1.0 / bufferLength
+  let x = 0
+  for (let i = 0; i < bufferLength; i++) {
+    let v = dataArray[i] / 128.0
+    let y = v * H / 2
+    if (i === 0) {
+      ctx.moveTo(x, y)
+    } else {
+      ctx.lineTo(x, y)
+    }
+    x += sliceWidth
+  }
+  ctx.lineTo(W, H / 2)
+  ctx.stroke()
+  requestAnimationFrame(draw)
+}
+
+const draw2 = () => {
+  // 获取当前频域数据；将当前频域数据拷贝进Uint8Array数组（无符号字节数组）
+  analyser.getByteFrequencyData(dataArray)
+  ctx.clearRect(0, 0, W, H)
+  ctx.fillStyle = 'rgb(0,0,0)'
+  ctx.fillRect(0, 0, W, H)
+
+  const barWidth = (W / bufferLength) * 2.5
+  let barHeight
+  let x = 0
+
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i] / 2
+    ctx.fillStyle = `rgb(${barHeight + 100},50,50)`
+    ctx.fillRect(x, H - barHeight, barWidth, barHeight)
+    x += barWidth + 1
+  }
+
+  requestAnimationFrame(draw2)
+}
+
+window.onload = function () {
+  draw()
+}
