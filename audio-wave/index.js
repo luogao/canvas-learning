5c910a93ade0968434ea72482eb777e4d609ed4f
罗高 <luogao_lg@sina.com>
2018-08-25 22:59:16 +0800

    update audio-wave demo

--- audio-wave/index.js
+++ audio-wave/index.js
@@ -1,5 +1,4 @@
 import './index.css'
-
 import { createCanvas } from '../utils'
 
 const modeSelector = document.getElementById('mode')
@@ -11,6 +10,44 @@ modeSelector.addEventListener('change', function () {
     requestAnimationFrame(draw2)
   }
 }, false)
+
+
+navigator.getUserMedia = (navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia)
+
+
+const audioSourceByUser = (audioCtx, analyser) => {
+  if (navigator.getUserMedia) {
+    console.log('getUserMedia supported.');
+    navigator.getUserMedia(
+      // constraints - only audio needed for this app
+      {
+        audio: true
+      },
+      // Success callback
+      function (stream) {
+        const source = audioCtx.createMediaStreamSource(stream);
+        source.connect(analyser);
+        // analyser.connect(audioCtx.destination);
+        if (modeSelector.value === '1') {
+          requestAnimationFrame(draw)
+        } else {
+          requestAnimationFrame(draw2)
+        }
+      },
+      // Error callback
+      function (err) {
+        console.log('The following gUM error occured: ' + err);
+      }
+    );
+  } else {
+    console.log('getUserMedia not supported on your browser!');
+  }
+}
+
+
 const W = 500
 const H = 500
 
@@ -21,19 +58,19 @@ const myAudio = document.querySelector('audio')
 // 获取web audio 上下文对象
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 // 获取声音源
-const source = audioCtx.createMediaElementSource(myAudio)
+// const source = audioCtx.createMediaElementSource(myAudio)
 // 获取分析对象
 const analyser = audioCtx.createAnalyser()
 // 设置
-analyser.fftSize = 256
+analyser.fftSize = 1024
 const bufferLength = analyser.fftSize
 
 const dataArray = new Uint8Array(bufferLength)
 
 // 连接解析器
-source.connect(analyser)
-// 输出音频
-source.connect(audioCtx.destination)
+// source.connect(analyser)
+// // 输出音频
+// source.connect(audioCtx.destination)
 
 const draw = () => {
   // 获取当前声音的波形；将当前波形，或者时域数据拷贝进 Uint8Array数组（无符号字节数组）
@@ -82,5 +119,5 @@ const draw2 = () => {
 }
 
 window.onload = function () {
-  draw()
+  audioSourceByUser(audioCtx, analyser)
 }
