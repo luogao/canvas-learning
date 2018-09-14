6faaab842c1d932154cb2915d59216f8ba67f054
luogao <luogao_lg@sina.com>
2018-09-14 16:35:17 +0800

    finished snake game

--- snake-game/index.js
+++ snake-game/index.js
@@ -4,6 +4,8 @@ const CANVAS_BACKGROUND_COLOR = '#fff'
 const CANVAS_BORDER_COLOUR = '#000'
 const SNAKE_BODY_COLOR = 'lightgreen'
 const SNAKE_BODY_LINE_COLOR = 'darkgreen'
+const FOOD_COLOR = 'red'
+const FOOD_BORDER_COLOR = 'darkred'
 const LEFT_KEY = 37
 const RIGHT_KEY = 39
 const UP_KEY = 38
@@ -16,6 +18,9 @@ const ctx = gameCavans.getContext('2d')
 let dx = 10
 let dy = 0
 let changingDirection = false
+let foodX = 0
+let foodY = 0
+let score = 0
 
 let snake = [
   { x: 150, y: 150 },
@@ -24,6 +29,41 @@ let snake = [
   { x: 120, y: 150 },
   { x: 110, y: 150 }
 ]
+
+const randomTen = function (min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10
+}
+
+const createFood = function () {
+  foodX = randomTen(0, W - 10)
+  foodY = randomTen(0, H - 10)
+
+  snake.forEach((part) => {
+    const foodIsOnSnake = part.x === foodX && part.y === foodY
+    if (foodIsOnSnake) createFood()
+  })
+}
+
+const didGameEnd = function () {
+  for (let i = 4; i < snake.length; i++) {
+    const didCollide = snake[i].x === snake[0].x && snake[i].y === snake[0].y
+    if (didCollide) return true
+  }
+
+  const hitLeftWall = snake[0].x < 0
+  const hitRightWall = snake[0].x > W - 10
+  const hitTopWall = snake[0].y < 0
+  const hitBottomWall = snake[0].y > H - 10
+
+  return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
+}
+
+const drawFood = function () {
+  ctx.fillStyle = FOOD_COLOR
+  ctx.strokeStyle = FOOD_BORDER_COLOR
+  ctx.fillRect(foodX, foodY, 10, 10)
+  ctx.strokeRect(foodX, foodY, 10, 10)
+}
 
 const changeDirection = function (event) {
   const keyPressed = event.keyCode
@@ -63,7 +103,15 @@ const initCanvas = function () {
 const advanceSnake = function (snake) {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy }
   snake.unshift(head)
-  snake.pop()
+
+  const didEatFood = snake[0].x === foodX && snake[0].y === foodY
+  if (didEatFood) {
+    score += 10
+    document.getElementById('score').innerHTML = score
+    createFood()
+  } else {
+    snake.pop()
+  }
 }
 
 const drawSnakePart = function (snakePark) {
@@ -79,15 +127,21 @@ const drawSnake = function () {
 }
 
 const start = function () {
-  let timer = setTimeout(() => {
+  if (didGameEnd()) {
+    document.querySelector('.welcome').innerHTML = 'Game Over'
+    return
+  }
+  setTimeout(() => {
     changingDirection = false
     initCanvas()
+    drawFood()
     advanceSnake(snake)
     drawSnake()
     start()
   }, 100)
+
 }
 
 document.addEventListener('keydown', changeDirection)
-
+createFood()
 start()
