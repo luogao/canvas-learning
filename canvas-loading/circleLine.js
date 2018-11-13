9875171f87fae24135f286f43b115c0597adc097
luogao <luogao_lg@sina.com>
2018-11-13 17:54:58 +0800

    新建CircleLine 类 优化动画处理 加入回调

---
+++ canvas-loading/circleLine.js
@@ -0,0 +1,104 @@
+class CircleLine {
+  constructor({
+    canvas,
+    ctx,
+    center,
+    radius,
+    width,
+    color,
+    startAngle,
+    clockwise = true,
+    animation,
+    howLong
+  }) {
+    if (howLong.total < howLong.length) {
+      console.warn("length should less than total");
+      return;
+    }
+    this.rad = (Math.PI * 2) / howLong.total;
+    const endAngle = startAngle + this.rad * howLong.length;
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.center = center;
+    this.radius = radius;
+    this.width = width;
+    this.color = color;
+    this.startAngle = startAngle;
+    this.endAngle = endAngle;
+    this.clockwise = clockwise;
+    this.animation = animation;
+    this.howLong = howLong;
+    this.timer = null;
+    this.step = 0;
+    this.speed = 0;
+    this._endAngle = startAngle;
+    this.isAnimating = false;
+  }
+
+  render() {
+    if (this.animation && this.animation.duration) {
+      const interval = 1000 / 60;
+      this.speed = this.howLong.length / (this.animation.duration / interval);
+      console.time("animation time");
+      this._animate();
+    } else {
+      this._draw();
+    }
+  }
+
+  _animate() {
+    this.isAnimating = true;
+    this._endAngle = this.startAngle + this.rad * this.step;
+    this.step += this.speed;
+    clearCanvas(this.canvas, this.ctx);
+    this._draw(this._endAngle);
+    if (this.step <= this.howLong.length) {
+      this.timer = requestAnimationFrame(this._animate.bind(this));
+    } else {
+      console.timeEnd("animation time");
+      cancelAnimationFrame(this.timer);
+      clearCanvas(this.canvas, this.ctx);
+      this._draw();
+      this.isAnimating = false;
+      typeof this.animation.end === "function" && this.animation.end();
+    }
+  }
+
+  _draw(endAngle) {
+    drawCircleLine({
+      ctx: this.ctx,
+      center: this.center,
+      radius: this.radius,
+      color: this.color,
+      width: this.width,
+      startAngle: this.startAngle,
+      endAngle: endAngle || this.endAngle,
+      clockwise: !this.clockwise
+    });
+  }
+}
+
+const clearCanvas = (canvas, ctx) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+const drawCircleLine = ({
+  ctx,
+  center = { x: 0, y: 0 },
+  radius,
+  color,
+  width,
+  startAngle = 0,
+  endAngle = Math.PI * 2,
+  clockwise = false
+}) => {
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.strokeStyle = color;
+  ctx.arc(center.x, center.y, radius, startAngle, endAngle, clockwise);
+  ctx.stroke();
+  ctx.restore();
+};
+
+export default CircleLine;
--- canvas-loading/index.js
+++ canvas-loading/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import CircleLine from "./circleLine";
 
 const circleLoadingCanvas = document.getElementById("circle-loading");
 const circleLoadingContext = circleLoadingCanvas.getContext("2d");
@@ -24,102 +25,6 @@ const loadingTrack = {
   color: "#ccc"
 };
 
-const drawCircleLine = ({
-  ctx,
-  center = { x: 0, y: 0 },
-  radius,
-  color,
-  width,
-  startAngle = 0,
-  endAngle = Math.PI * 2,
-  clockwise = false
-}) => {
-  ctx.save();
-  ctx.beginPath();
-  ctx.lineWidth = width;
-  ctx.strokeStyle = color;
-  ctx.arc(center.x, center.y, radius, startAngle, endAngle, clockwise);
-  ctx.stroke();
-  ctx.restore();
-};
-
-const drawLoadingTrack = ({
-  canvas,
-  ctx,
-  center = { x: 0, y: 0 },
-  radius,
-  color,
-  width,
-  startAngle = 0,
-  howLong = {
-    total: 100,
-    length: 0
-  },
-  clockwise = false,
-  animation = null
-}) => {
-  if (howLong.total < howLong.length) {
-    console.warn("length should less than total");
-    return;
-  }
-  let rad = (Math.PI * 2) / howLong.total;
-  let endAngle = startAngle + rad * howLong.length;
-  if (animation && animation.duration) {
-    const interval = 1000 / 60;
-    let timer;
-    let step = 0;
-    let speed = howLong.length / (animation.duration / interval);
-    let _endAngle = startAngle;
-    const animate = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      // 计算角度
-      _endAngle = startAngle + rad * step;
-      step += speed;
-      // 绘制圆圈
-      drawCircleLine({
-        ctx,
-        center,
-        radius,
-        width,
-        color: "#000",
-        startAngle,
-        endAngle: _endAngle,
-        clockwise
-      });
-
-      if (step <= howLong.length) {
-        timer = requestAnimationFrame(animate);
-      } else {
-        cancelAnimationFrame(timer);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawCircleLine({
-          ctx,
-          center,
-          radius,
-          width,
-          color: "#000",
-          startAngle,
-          endAngle,
-          clockwise
-        });
-      }
-    };
-
-    animate();
-  } else {
-    drawCircleLine({
-      ctx,
-      center,
-      radius,
-      width,
-      color,
-      startAngle,
-      endAngle,
-      clockwise
-    });
-  }
-};
-
 const drawLoadingBar = progress => {
   circleLoadingContext.save();
   circleLoadingContext.beginPath();
@@ -137,23 +42,43 @@ const drawLoadingBar = progress => {
   circleLoadingContext.restore();
 };
 
-setTimeout(() => {
-  drawLoadingTrack({
-    canvas: circleLoadingCanvas,
-    ctx: circleLoadingContext,
-    center,
-    radius: loading.size,
-    width: loadingTrack.width,
-    color: loadingTrack.color,
-    startAngle: -Math.PI / 2,
-    howLong: {
-      total: 100,
-      length: 99
-    },
-    animation: {
-      duration: 3000
+const c1 = new CircleLine({
+  canvas: circleLoadingCanvas,
+  ctx: circleLoadingContext,
+  center,
+  radius: loading.size,
+  width: loadingTrack.width,
+  color: loadingTrack.color,
+  startAngle: -Math.PI / 2,
+  clockwise: true,
+  howLong: {
+    total: 100,
+    length: 100
+  }
+});
+
+const c2 = new CircleLine({
+  canvas: circleLoadingCanvas,
+  ctx: circleLoadingContext,
+  center,
+  radius: loading.size,
+  width: loadingBar.width,
+  color: loadingBar.color,
+  startAngle: -Math.PI / 2,
+  clockwise: true,
+  howLong: {
+    total: 100,
+    length: 10
+  },
+  animation: {
+    duration: 1000,
+    end() {
+      console.log("animation finished!");
     }
-  });
-}, 500);
+  }
+});
+
+c1.render();
+c2.render();
 
 // drawLoadingBar(1);
