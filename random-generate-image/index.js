589c3864b8d0efc9f222298c8ba0f7d80e48964f
luogao <roy.luo@ximalaya.com>
2019-10-15 13:32:26 +0800

    [update] 添加保存图片功能

--- random-generate-image/index.js
+++ random-generate-image/index.js
@@ -1,21 +1,17 @@
 import './index.css'
+import downloadFile from './lib/downloadCanvas'
 
 const CANVAS_SIZE = 600
-const step = 10
+const step = 20
 
 const colorArr = ['#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffd369', '#e26241', '#940a37', '#5b0909', '#00818a', '#ec9b3b']
 const randomColor = `#${(~~(Math.random() * (1 << 24))).toString(16)}`
 const canvas = document.getElementById('canvas')
+const saveBtn = document.getElementById('save-btn')
+
 const ctx = canvas.getContext('2d')
 canvas.width = CANVAS_SIZE
 canvas.height = CANVAS_SIZE
-
-// for (let x = 0; x < CANVAS_SIZE; x += 5) {
-//   for (let y = 0; y < CANVAS_SIZE; y += 5) {
-//     ctx.fillStyle = `#${(~~(Math.random() * (1 << 24))).toString(16)}`
-//     ctx.fillRect(x, y, 5, 5)
-//   }
-// }
 
 let x = 0
 let y = 0
@@ -31,7 +27,30 @@ const draw = () => {
   }
   if (x < CANVAS_SIZE && y < CANVAS_SIZE) {
     requestAnimationFrame(draw)
+  } else {
+    saveBtn.style.display = 'initial'
   }
 }
 
-draw()
+const drawImmediately = (step) => {
+  for (let x = 0; x < CANVAS_SIZE; x += step) {
+    for (let y = 0; y < CANVAS_SIZE; y += step) {
+      ctx.fillStyle = colorArr[Math.floor(Math.random() * colorArr.length)]
+      ctx.fillRect(x, y, step, step)
+    }
+  }
+  saveBtn.style.display = 'initial'
+}
+
+const handleDownload = () => {
+  console.log('??')
+  downloadFile('good-lucky', canvas)
+}
+
+
+window.onload = function() {
+  saveBtn.style.display = 'none'
+  saveBtn.addEventListener('click', handleDownload)
+  // draw()
+  drawImmediately(step)
+}
